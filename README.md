e8b32fa4fd44294cd5b6a2e56ca648d4439f244f
lixk <1749498702@qq.com>
2021-03-27 22:04:36 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ceo
+# eui
+a fast and simple micro-framework for small browser-based applications
+
+### example
+server:
+```python
+import eui
+import time
+
+
+def get_time():
+    return str(time.time())
+
+
+service = {'get_time': get_time}
+
+eui.run(service, static_dir='web/')
+
+```
+UI
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="eui.js"></script>
+</head>
+<body>
+<script>
+eui.run('get_time', {}, function(data){
+	    console.log(data);
+	  });
+</script>
+</body>
+</html>
+
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="eui",
+    version="0.0.2",
+    keywords=["pip", "eui"],
+    description="a fast and simple micro-framework for small web applications",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    license="MIT Licence",
+
+    url="https://github.com/lixk/eui",
+    author="Xiangkui Li",
+    author_email="1749498702@qq.com",
+    py_modules=['eui'],
+    # packages=find_packages(),
+    # include_package_data=True,
+    platforms="any",
+    install_requires=["aiohttp", "aiohttp_cors"]
+)
